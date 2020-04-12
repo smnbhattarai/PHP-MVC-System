e03c145632fd571e15097180cb32c2f4cab53b57
suman <suman.bhattarai@gmail.com>
2020-04-12 16:32:16 +0545

    Route dispatch added

---
+++ App/Controllers/Posts.php
@@ -0,0 +1,16 @@
+<?php
+
+class Posts
+{
+
+    public function index()
+    {
+        echo "Hello from index of posts.";
+    }
+
+    public function addNew()
+    {
+        echo 'Hello from addNew on Posts';
+    }
+
+}
--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@
 //echo 'Request URL = ' . $_SERVER['QUERY_STRING'];
 
 require_once '../Core/Router.php';
+require_once '../App/Controllers/Posts.php';
 
 $router = new Router();
 
@@ -16,20 +17,4 @@ $router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
 $router->add('{controller}/{action}');
 $router->add('{controller}/{id:\d+}/{action}');
 
-// Display the routing table
-echo '<pre>';
-//var_dump($router->getRoutes());
-echo htmlspecialchars(print_r($router->getRoutes(), true));
-echo '</pre>';
-
-
-// Match the requested route
-$url = $_SERVER['QUERY_STRING'];
-
-if ($router->match($url)) {
-    echo '<pre>';
-    var_dump($router->getParams());
-    echo '</pre>';
-} else {
-    echo "No route found for URL '$url'";
-}
+$router->dispatch($_SERVER['QUERY_STRING']);
