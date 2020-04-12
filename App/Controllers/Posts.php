f4e192179488e85dcfa48f1f9d44b0dfcf718a7f
suman <suman.bhattarai@gmail.com>
2020-04-12 23:16:10 +0545

    Moved class autoload to composer

--- App/Controllers/Posts.php
+++ App/Controllers/Posts.php
@@ -2,25 +2,24 @@
 
 namespace App\Controllers;
 
+use \Core\View;
+
 class Posts extends \Core\Controller
 {
 
     public function indexAction()
     {
-        echo "Hello from index of posts.<hr>";
-        echo htmlspecialchars(print_r($_GET, true));
+        View::renderTemplate('Posts/index.html');
     }
 
     public function addNewAction()
     {
-        echo 'Hello from addNew on Posts';
 
     }
 
     public function editAction()
     {
-        echo 'Hello from Posts edit. <hr>';
-        echo htmlspecialchars(print_r($this->route_params, true));
+
     }
 
 }
--- public/index.php
+++ public/index.php
@@ -5,18 +5,6 @@
  */
 require __DIR__ . '/../vendor/autoload.php';
 
-
-/**
- * Classes Autoloader
- */
-spl_autoload_register(function ($class) {
-    $root = dirname(__DIR__);
-    $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
-    if (is_readable($file)) {
-        require $root . '/' . str_replace('\\', '/', $class) . '.php';
-    }
-});
-
 /**
  * Routing
  */
